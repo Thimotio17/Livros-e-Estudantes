5f13f04150a9410bbb712669d9d48b29c9f6141c
Lucas Thimotio <160185047+Thimotio17@users.noreply.github.com>
2024-09-03 18:34:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// Importando as rotas
+
 const livrosRouter = require('./routes/livros');
 const estudantesRouter = require('./routes/estudantes');
 const alugueisRouter = require('./routes/alugueis');
@@ -9,15 +9,15 @@ const alugueisRouter = require('./routes/alugueis');
 const app = express();
 const PORT = 3000;
 
-// Configurando o body-parser para lidar com requisições JSON
+
 app.use(bodyParser.json());
 
-// Configurando as rotas
+
 app.use('/livros', livrosRouter);
 app.use('/estudantes', estudantesRouter);
 app.use('/alugueis', alugueisRouter);
 
-// Iniciando o servidor
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
