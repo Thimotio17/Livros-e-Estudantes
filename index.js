df12dd181792a6fbc6ba3f9627dcbb1e09a94908
Lucas Thimotio <160185047+Thimotio17@users.noreply.github.com>
2024-09-03 14:59:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,155 +1,25 @@
-const prompt = require("prompt-sync")();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const corretora = require("./modulos/corretora.js");
-const cliente = require("./modulos/cliente.js");
-const corretor = require("./modulos/corretor.js");
+// Importando as rotas
+const livrosRouter = require('./routes/livros');
+const estudantesRouter = require('./routes/estudantes');
+const alugueisRouter = require('./routes/alugueis');
 
-const menuPrincipal = () => {
-  console.log(`
-        GERENCIAMENTO DE CORRETORA DE IMÓVEIS
-        `);
+const app = express();
+const PORT = 3000;
 
-  while (true) {
-    console.log(`
-                  ESCOLHA O MÓDULO QUE QUER GERENCIAR:
-                  
-                  1 - CORRETORA
-                  2 - CLIENTE
-                  3 - CORRETOR
-                  0 - SAIR
-                  `);
+// Configurando o body-parser para lidar com requisições JSON
+app.use(bodyParser.json());
 
-    const opcaoModulo = parseInt(prompt(": "));
+// Configurando as rotas
+app.use('/livros', livrosRouter);
+app.use('/estudantes', estudantesRouter);
+app.use('/alugueis', alugueisRouter);
 
-    switch (opcaoModulo) {
-      case 1:
-        menuCorretora();
-        break;
+// Iniciando o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
 
-      case 2:
-        menuCliente();
-        break;
 
-      case 3:
-        menuCorretor();
-        break;
-
-      case 0:
-        process.exit();
-        break;
-      default:
-        console.log("OPCAO INVALIDA");
-        break;
-    }
-  }
-};
-
-const menuCliente = () => {
-  console.log("GERENCIAMENTO DE CLIENTE");
-  while (true) {
-    console.log(`
-                1 - CADASTRAR CLIENTE
-                2 - LISTAR CLIENTES
-                3 - ATUALIZAR CLIENTE
-                4 - EXCLUIR CLIENTE
-                0 - SAIR
-                `);
-
-    const opcaoServico = parseInt(prompt(": "));
-
-    switch (opcaoServico) {
-      case 1:
-        cliente.store();
-        break;
-      case 2:
-        cliente.index();
-        break;
-      case 3:
-        cliente.update();
-        break;
-      case 4:
-        cliente.destroy();
-        break;
-
-      case 0:
-        return;
-      default:
-        console.log("OPCAO INVALIDA");
-        break;
-    }
-  }
-};
-
-const menuCorretor = () => {
-  console.log("GERENCIAMENTO DE CORRETOR");
-  while (true) {
-    console.log(`
-                1 - CADASTRAR CORRETOR
-                2 - LISTAR CORRETORES
-                3 - ATUALIZAR CORRETOR
-                4 - EXCLUIR CORRETOR
-                0 - SAIR
-                `);
-
-    const opcaoServico = parseInt(prompt(": "));
-
-    switch (opcaoServico) {
-      case 1:
-        corretor.store();
-        break;
-      case 2:
-        corretor.index();
-        break;
-      case 3:
-        corretor.update();
-        break;
-      case 4:
-        corretor.destroy();
-        break;
-
-      case 0:
-        return;
-      default:
-        console.log("OPCAO INVALIDA");
-        break;
-    }
-  }
-};
-
-const menuCorretora = () => {
-  console.log("GERENCIAMENTO DE CORRETORA");
-  while (true) {
-    console.log(`
-              1 - CADASTRAR CORRETORA
-              2 - LISTAR CORRETORAS
-              3 - ATUALIZAR CORRETORA
-              4 - EXCLUIR CORRETORA
-              0 - SAIR
-              `);
-
-    const opcaoServico = parseInt(prompt(": "));
-
-    switch (opcaoServico) {
-      case 1:
-        corretora.store();
-        break;
-      case 2:
-        corretora.index();
-        break;
-      case 3:
-        corretora.update();
-        break;
-      case 4:
-        corretora.destroy();
-        break;
-
-      case 0:
-        return;
-      default:
-        console.log("OPCAO INVALIDA");
-        break;
-    }
-  }
-};
-
-menuPrincipal();
